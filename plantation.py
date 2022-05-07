17af3dfdd20d64e40b7ffddea464eac8db9c888c
demetriugabriel <demetriugabriel2011@hotmail.com>
2022-05-07 01:19:16 -0300

    update plantation, amount of watter and need watter def

--- plantation.py
+++ plantation.py
@@ -1,57 +1,59 @@
 import pygame
 import time
-from main import SCREEN_W, SCREEN_H
+from mouse import Mouse
+from main import SCREEN_W, SCREEN_H, screen
 
 GREEN = (34, 139, 34)
 BLUE  = (161, 255, 254)
 
-earth_rect = pygame.rect(SCREEN_W//3, SCREEN_H//3, SCREEN_W//3, SCREEN_H)
+earth_rect = pygame.rect(SCREEN_W//3, SCREEN_H//3, 16, 16)
 color_earth_rect = BLUE
 
-class Terra_fertil:
+class Plantation:
     def __init__(self):
-        self.quant_agua = 100 #porcentagem_agua abaixo de 50% a terra já pede
-        #self.evolucao = 0 # 0 a 100%
-        self.semente_recebida = None
-        self.planta_coletar = None
+        self.amountWaterNeeded = 100 #porcentagem_agua abaixo de 50% a terra já pede
+        self.seeds_recive = None
+        self.plantCollect = None
 
     def draw(self):
         pygame.draw.rect((SCREEN_W, SCREEN_H), color_earth_rect, earth_rect)
     
-    def receive_seed(self, semente):
-        self.semente_recebida = semente
-        self.tempo_recebido = time.time()
+    def receiveSeed(self, seeds):
+        self.seeds_recive = seeds
+        self.time_received = time.time()
 
-    """def receber_agua(self, quantidade_agua):
-        self.quant_agua = quantidade_agua
+    def reciveWatter(self, amountWaterNeeded):
+        self.amountOfWatter = amountWaterNeeded
 
-    def pedir_agua(self):
-        if self.quant_agua <= 50:
+    def needWater(self):
+        if self.amountOfWatter <= 50:
             print('preciso de agua')
 
-    def mudar_imagem(self):
-        if Semente.evolucao >= 25:
+    """def mudar_imagem(self):
+        if seeds.evolucao >= 25:
             #imagem1...
             pass"""
 
-    def collect_plant(self, semente):
-        self.planta_coletar = semente
+    def collectPlant(self, seeds):
+        self.plantCollect = seeds
     
     def loop(self):
-        if self.semente_recebida != None:
-            tempo_passado = time.time() - self.tempo_recebido
-            if tempo_passado >= self.semente_recebida.tempo_colher:
+        if self.seeds_recive != None:
+            tempo_passado = time.time() - self.time_received
+            if tempo_passado >= self.seeds_recive.tempo_colher:
                 print('planta madura')
-                self.semente_recebida.evolucao = 100
-            elif tempo_passado < self.semente_recebida.tempo_colher:
+                self.seeds_recive.evolucao = 100
+            elif tempo_passado < self.seeds_recive.tempo_colher:
                 print('planta crescendo')
         
-        if self.planta_coletar != None:
-            if self.planta_coletar.evolucao == 100:
-                for event in pygame.event.get():
-                    pos = pygame.mouse.get_pos()
-                    if event.type == pygame.MOUSEBUTTONDOWN:
-                        if earth_rect.collidepoint(pos):
-                            print('PLANTA COLETADA')
-                            self.semente_recebida = None
-                            self.planta_coletar = None
+        if self.plantCollect != None:
+            if self.plantCollect.evolucao == 100:
+                #pygame.blit(screen, img ,earth_rect)
+                print('PLANTA COLETADA')
+                self.seeds_recive = None
+                self.plantCollect = None
+
+        if self.amountOfWatter != None:
+            if tempo_passado >= 10:
+                self.amountOfWatter -= 10
+                self.amountOfWatter = None

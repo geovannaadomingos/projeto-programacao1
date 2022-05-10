36b5aae39c1d28b3731d7ee416a884cb3f93d111
lucasccampos <campops94@gmail.com>
2022-05-10 20:26:28 -0300

    fix farmer

--- farmer.py
+++ farmer.py
@@ -16,11 +16,12 @@ class Farmer(GameObject):
 
     def __init__(self, v2_pos, speed, frameDuration=6):
         self.animations = datamanager.DataManager.PLAYER_ANIMATIONS
-        super().__init__(v2_pos, Vector2.FromList(self.animations["idle_left"][0].get_size()), clickable=False)
+        super().__init__(v2_pos, Vector2.FromList(
+            self.animations["idle_left"][0].get_size()), clickable=False)
 
         self.speed = speed
         self.v2_targetPos = None
-        self.v2_direction = Vector2(0,0)
+        self.v2_direction = Vector2(0, 0)
         self.arriveEvent = None
 
         self.frameCount = 0
@@ -31,11 +32,12 @@ class Farmer(GameObject):
         self.len_itens_inventory = 0
         self.selectedInventoryIndex = 0
 
-        self.v2_collideBox = Vector2(16,16) * (self.v2_size.x//48) * 0.5
-        self.v2_collideOffset = (self.getCenterPos() - (self.v2_collideBox/2)) - self.v2_pos
+        self.v2_collideBox = Vector2(16, 16) * (self.v2_size.x//48) * 0.5
+        self.v2_collideOffset = (
+            self.getCenterPos() - (self.v2_collideBox/2)) - self.v2_pos
 
         self.surface = pygame.Surface(self.v2_collideBox)
-        self.surface.fill((0,0,0))
+        self.surface.fill((0, 0, 0))
 
         self.cronometerWalking = 0
 
@@ -44,34 +46,34 @@ class Farmer(GameObject):
         self.targetPathIndex = 0
 
         self.addToInventory(WateringCan())
-        
+
     def loop(self):
         self.move()
 
         self.frameCount += 1
         if self.frameCount == self.frameDuration * 8:
             self.frameCount = 0
-        
+            
+            if self.state == "enxada":
+                self.arar()
+            elif self.state == "regador":
+                self.regar()
+
         if self.state == "walking":
             self.cronometerWalking += gamemanager.GameManager.deltaTime
             if self.cronometerWalking >= 1:
                 self.playStepSounds()
 
-    def playStepSounds(self):
-        Sounds.playSFX("step.wav")
-        self.cronometerWalking = 0
-
-        if self.state == "enxada":
-            self.arar()
-        elif self.state == "regador":
-            self.regar()
-
         for event in Events.events:
             if event.type == pygame.KEYDOWN:
                 if event.key in range(49, 58):
                     self.selectedInventoryIndex = (event.key - 49)
-                    print(f"({self.selectedInventoryIndex}) Item atual >: {type(self.getCurrentItem())}")
+                    print(
+                        f"({self.selectedInventoryIndex}) Item atual >: {type(self.getCurrentItem())}")
 
+    def playStepSounds(self):
+        Sounds.playSFX("step.wav")
+        self.cronometerWalking = 0
 
     def getCurrentItem(self):
         try:
@@ -79,31 +81,29 @@ class Farmer(GameObject):
         except:
             return None
 
-
     def handleClick(self, gameObject, v2_mousePos):
-        if gamemanager.GameManager.grid != None:
-            if type(gameObject) == Plantation:
-                currentItem = self.getCurrentItem()
-                if type(currentItem) == SeedItem:
-                    if gameObject.canReceiveSeed():
-                        self.moveTo(v2_mousePos, gameObject, self.ararAnimation)
-                elif type(currentItem) == WateringCan:
-                    if gameObject.seed != None:
-                        self.moveTo(v2_mousePos, gameObject, self.regarAnimation)
-                    else:
-                        self.moveTo(v2_mousePos)
-                elif currentItem == None:
-                    if gameObject.canReceiveSeed():
-                        self.moveTo(v2_mousePos)
+        if type(gameObject) == Plantation:
+            currentItem = self.getCurrentItem()
+            if type(currentItem) == SeedItem:
+                if gameObject.canReceiveSeed():
+                    self.moveTo(v2_mousePos, gameObject, self.ararAnimation)
+            elif type(currentItem) == WateringCan:
+                if gameObject.seed != None:
+                    self.moveTo(v2_mousePos, gameObject, self.regarAnimation)
+                else:
+                    self.moveTo(v2_mousePos)
+            elif currentItem == None:
+                if gameObject.canReceiveSeed():
+                    self.moveTo(v2_mousePos)
 
-
-            else:
-                self.moveTo(v2_mousePos)
+        else:
+            self.moveTo(v2_mousePos)
 
     def move(self):
         try:
             if self.state == "walking" and self.v2_targetPos != None:
-                self.v2_direction = (self.targetPath[self.targetPathIndex] - self.getCenterPos())
+                self.v2_direction = (
+                    self.targetPath[self.targetPathIndex] - self.getCenterPos())
                 if self.v2_direction.magnitude() > self.speed:
                     self.v2_pos += self.v2_direction.normalize() * self.speed
                 else:
@@ -114,7 +114,6 @@ class Farmer(GameObject):
                         self.move()
         except Exception as e:
             print(f"Error {e}")
-
 
     def arrived(self):
         self.changeState("idle")
@@ -136,26 +135,36 @@ class Farmer(GameObject):
             else:
                 direction = "down"
 
-        screen.blit(self.animations[self.state+"_"+direction][self.frameCount // self.frameDuration], self.v2_pos)
+        screen.blit(self.animations[self.state+"_"+direction]
+                    [self.frameCount // self.frameDuration], self.v2_pos)
         # screen.blit(self.surface, self.v2_pos + self.v2_collideOffset)
 
     def changeState(self, newState):
+        print(newState)
+        if(self.state != newState):
+            self.frameCount = 0
         self.state = newState
-        self.frameCount = 0
 
     def moveTo(self, v2_targetPos, gameObject=None, eventHandler=None):
+        if gamemanager.GameManager.grid == None:
+            return
+
         if v2_targetPos != None:
-            nodeFarmer = gamemanager.GameManager.grid.getNodeFromPoint(self.getCenterPos())
-            nodeDestino = gamemanager.GameManager.grid.getNodeFromPoint(v2_targetPos)
+            nodeFarmer = gamemanager.GameManager.grid.getNodeFromPoint(
+                self.getCenterPos())
+            nodeDestino = gamemanager.GameManager.grid.getNodeFromPoint(
+                v2_targetPos)
 
             if nodeFarmer == nodeDestino:
                 return
 
-            path = Pathfinding.get_path(nodeFarmer, nodeDestino, gamemanager.GameManager.grid)
+            path = Pathfinding.get_path(
+                nodeFarmer, nodeDestino, gamemanager.GameManager.grid)
             caminho = []
             if len(path) > 0:
                 for node in path:
-                    caminho.append(gamemanager.GameManager.grid.getNodeScreenPosCenter(node))
+                    caminho.append(
+                        gamemanager.GameManager.grid.getNodeScreenPosCenter(node))
 
                 self.v2_targetPos = caminho[-1]
 
@@ -179,14 +188,16 @@ class Farmer(GameObject):
 
     def ararAnimation(self):
         self.changeState("enxada")
+        Sounds.playSFX("dig.wav")
         if self.v2_targetPos != None:
             self.v2_direction = (self.v2_targetPos - self.getCenterPos())
-    
+
     def regarAnimation(self):
         self.changeState("regador")
+        Sounds.playSFX("water.wav")
         if self.v2_targetPos != None:
             self.v2_direction = (self.v2_targetPos - self.getCenterPos())
-    
+
     def regar(self):
         self.getCurrentItem().water(self.targetObject)
         self.changeState("idle")
@@ -206,4 +217,4 @@ class Farmer(GameObject):
             if self.inventory[index] == None:
                 break
         self.len_itens_inventory += 1
-        self.inventory[index] = item 
+        self.inventory[index] = item
